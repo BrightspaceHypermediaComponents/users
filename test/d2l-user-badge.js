7d091fa50a4e253eee8e4dab6812dc43fb21ea0d
Josh Roach <josh.roach@D2L.com>
2019-03-12 15:50:27 -0400

    Initial work on unit tests

---
+++ test/d2l-user-badge.js
@@ -0,0 +1,17 @@
+(function() {
+	let userBadge;
+
+	suite('d2l-user-badge base', function() {
+		setup(function() {
+			userBadge = fixture('base');
+		});
+		test('instantiating the element works', function() {
+			assert.equal(userBadge.tagName.toLowerCase(), 'd2l-user-badge');
+		});
+		test('attributes are set correctly', function() {
+			assert.equal(userBadge.href, 'data/user.json');
+			assert.equal(userBadge.displayNameHref, 'https://www.d2l.com');
+			assert.equal(userBadge.token, 'faketoken');
+		});
+	});
+})();
