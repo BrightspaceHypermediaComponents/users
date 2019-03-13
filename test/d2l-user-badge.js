4f8faafe2f250dbfbf0ab849492cbf70101df822
Josh Roach <josh.roach@D2L.com>
2019-03-13 09:44:37 -0400

    Unit tests done

--- test/d2l-user-badge.js
+++ test/d2l-user-badge.js
@@ -1,6 +1,11 @@
 (function() {
 	let userBadge;
 
+	async function loadPromise(url) {
+		const entity = await window.D2L.Siren.EntityStore.fetch(url, '');
+		await userBadge._loadData(entity.entity);
+	}
+
 	suite('d2l-user-badge base', function() {
 		setup(function() {
 			userBadge = fixture('base');
@@ -12,6 +17,12 @@
 			assert.equal(userBadge.href, 'data/user.json');
 			assert.equal(userBadge.displayNameHref, 'https://www.d2l.com');
 			assert.equal(userBadge.token, 'faketoken');
+		});
+		test('displayName is set correctly', (done) => {
+			loadPromise('data/user.json').then(function() {
+				assert.equal(userBadge._displayName, 'Josh Roach');
+				done();
+			});
 		});
 	});
 })();
